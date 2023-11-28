77a9dfbb1219c6c966aef7fb2ae209a87020f2ab
Calan Meechang <calan.meechang@gmail.com>
2023-11-28 15:14:28 +1300

    Game can now be played more than once if player presses Enter after finishing a game

--- apple_game.py
+++ apple_game.py
@@ -1,119 +1,135 @@
 import pygame
+from apple_game_constants import *
 from apple_game_sprites import *
-
 pygame.init()
 
-SCREEN_WIDTH = 1350
-SCREEN_HEIGHT = 600
-
-white = (255, 255, 255)
-black = (0, 0, 0)
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 background = Background()
 tree = Tree()
-
-ADDAPPLE = pygame.USEREVENT + 1
-pygame.time.set_timer(ADDAPPLE, 250)
-
-SECOND = pygame.USEREVENT + 2
-pygame.time.set_timer(SECOND, 1000)
-
 player = Player()
 apples = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
-caught_apples = 0
-missed_apples = 0
-font = pygame.font.SysFont('Arial', 40)
+pygame.time.set_timer(ADDAPPLE, 250)
+pygame.time.set_timer(SECOND, 1000)
 
-clock = pygame.time.Clock()
-
-running = True
-paused = False
-game_over = False
-time = 60
-while running:
-    if paused:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    paused = False
-                    
-        text = font.render('Press Esc to continue', True, black, white)
-        width = text.get_width()
-        screen.blit(text, ((SCREEN_WIDTH/2) - (width/2), SCREEN_HEIGHT/2))
-        pygame.display.flip()
-
-    elif game_over:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                
-        text = font.render("Time's Up!", True, black, white)
-        width = text.get_width()
-        screen.blit(text, ((SCREEN_WIDTH/2) - (width/2), SCREEN_HEIGHT/2))
-        pygame.display.flip()
-        
-    else:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    paused = True
-
-            elif event.type == ADDAPPLE:
-                new_apple = Apple()
-                apples.add(new_apple)
-                all_sprites.add(new_apple)
-
-            elif event.type == SECOND:
-                time -= 1
-
-            elif time == 0:
-                game_over = True
-                
-        pressed_keys = pygame.key.get_pressed()
-        player.update(pressed_keys)
-        apples.update()
-        
-        screen.fill(black)
-        screen.blit(background.surf, background.rect)
-        for x in [-300, 500, 250, 10, 800]:
-            screen.blit(tree.surf, (x, -300))
-        
-        score = font.render(f'Score: {caught_apples}', True, black, white)
-        score_width = score.get_width()
-        missed = font.render(f'Missed: {missed_apples}', True, black, white)
-        missed_width = missed.get_width()
-        time_left = font.render(f'Time left: {time}', True, black, white)
+def game_loop():
+    running = True
+    paused = False
+    game_over = False
+    play_again = False
+    caught_apples = 0
+    missed_apples = 0
+    time = 60
+    while running:
+        caught, caught_width, caught_height = game_text(f'Caught: {caught_apples}')
+        missed, missed_width, missed_height = game_text(f'Missed: {missed_apples}')
+        time_left, time_width, time_height = game_text(f'Time left: {time}')
+        pause, pause_width, pause_height = game_text('Press Esc to pause')
         
         if caught_apples == 0 and missed_apples == 0:
-            percentage = font.render('0%', True, black, white)
+            percentage, _, _ = game_text('Score: 0%')
         else:
             percent = (caught_apples/(caught_apples + missed_apples)) * 100
-            percentage = font.render(f'{percent:.0f}%', True, black, white)
-        screen.blit(score, (0,0))
-        screen.blit(missed, (score_width+5, 0))
-        screen.blit(percentage, (score_width+missed_width+10, 0))
-        screen.blit(time_left, (0, 50))
-        
-        for entity in all_sprites:
-            screen.blit(entity.surf, entity.rect)
-        for apple in apples:
-            if apple.rect.bottom >= SCREEN_HEIGHT+10:
-                apple.kill()
-                missed_apples += 1
-        if pygame.sprite.spritecollide(player, apples, dokill=True):
-            caught_apples += 1
-              
-        pygame.display.flip()
-        clock.tick(70)
+            percentage, _, _ = game_text(f'Score: {percent:.0f}%')
+                
+        if paused:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        paused = False
+                        
+            pause, pause_width, pause_height = game_text('Press Esc to continue')
+            screen.blit(pause, ((SCREEN_WIDTH/2) - (pause_width/2), (SCREEN_HEIGHT/2) - (pause_height/2)))
+            pygame.display.flip()
+
+        elif game_over:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        play_again = True
+                        running = False
+
+            text1 = "Time's Up!"
+            text2 = f'You caught {caught_apples} out of {caught_apples+missed_apples} apples'
+            text3 = f'Your score is {percent:.0f}%'
+            text4 = 'Press Enter to play again'
+            timeup, timeup_width, timeup_height = game_text(text1)
+            result, result_width, result_height = game_text(text2)
+            score, score_width, score_height = game_text(text3)
+            again, again_width, again_height = game_text(text4)
+            screen.blit(timeup, ((SCREEN_WIDTH/2) - (timeup_width/2), (SCREEN_HEIGHT/2) - (result_height/2) - result_height))
+            screen.blit(result, ((SCREEN_WIDTH/2) - (result_width/2), (SCREEN_HEIGHT/2) - (result_height/2)))
+            screen.blit(score, ((SCREEN_WIDTH/2) - (score_width/2), (SCREEN_HEIGHT/2) - (result_height/2) + result_height))
+            screen.blit(again, ((SCREEN_WIDTH/2) - (again_width/2), (SCREEN_HEIGHT/2) - (result_height/2) + result_height*3))
+            pygame.display.flip()
+            
+        else:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        paused = True
+
+                elif event.type == ADDAPPLE:
+                    new_apple = Apple()
+                    apples.add(new_apple)
+                    all_sprites.add(new_apple)
+
+                elif event.type == SECOND:
+                    time -= 1
+
+            if time <= 0:
+                game_over = True
+                    
+            
+            screen.fill(BLACK)
+            screen.blit(background.surf, background.rect)
+            for x in [-300, 500, 250, 10, 800]:
+                screen.blit(tree.surf, (x, -300))
+
+            pressed_keys = pygame.key.get_pressed()
+            player.update(pressed_keys)
+            apples.update()
+
+            if not game_over:    
+                screen.blit(caught, (0,0))
+                screen.blit(missed, (caught_width+5, 0))
+                screen.blit(percentage, (caught_width+missed_width+10, 0))
+                screen.blit(time_left, (0, caught_height+5))
+
+            if not game_over and not paused:
+                screen.blit(pause, (SCREEN_WIDTH-pause_width, 0))
+            
+            for entity in all_sprites:
+                screen.blit(entity.surf, entity.rect)
+            for apple in apples:
+                if apple.rect.bottom >= SCREEN_HEIGHT+10:
+                    apple.kill()
+                    missed_apples += 1
+            if pygame.sprite.spritecollide(player, apples, dokill=True):
+                caught_apples += 1
+                  
+            pygame.display.flip()
+            clock.tick(70)
+            
+    return play_again
+
+play_again = game_loop()
+while play_again:
+    player = Player()
+    apples = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(player)
+
+    play_again = game_loop()
 
 pygame.quit()
